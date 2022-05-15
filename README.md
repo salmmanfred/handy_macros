62ab145ba290893b5cf3e2955a464fd1a90637c2
TheSlam <32799244+salmmanfred@users.noreply.github.com>
2022-05-15 02:20:56 +0200

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# handy_macros   
+  
+s!("&str") instead of "&str".to_string()  
+and  
+o!(borrowed_obj) instead of borrowed_obj.to_owned()
---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+#[macro_export]
+macro_rules! s {
+    ($e:expr) => {
+        $e.to_string()
+    };
+}
+#[macro_export]
+macro_rules! o {
+    ($e:expr) => {
+        $e.to_owned()
+    };
